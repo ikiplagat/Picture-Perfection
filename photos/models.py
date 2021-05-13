026a0813a28b15c3ef237fc96e02824a9ee4b9de
Kasparov <ianjkiplagat@gmail.com>
2021-05-13 15:09:50 +0300

    add photo uploads feature

--- photos/models.py
+++ photos/models.py
@@ -1,6 +1,28 @@
 from django.db import models
+from django.forms.fields import ImageField
 
-class Image(models.Model):
+class Category(models.Model):
   name = models.CharField(max_length =30)
-  description = models.CharField(max_length =50)
+
+  def __str__(self):
+      return self.name
+    
+class Location(models.Model):  
+  name = models.CharField(max_length =30)
+
+  def __str__(self):
+      return self.name  
+    
+class Image(models.Model):
+  image = models.ImageField(upload_to = 'images/')
+  name = models.CharField(max_length =30)
+  description = models.CharField(max_length =60)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+  location = models.ForeignKey(Location, on_delete=models.CASCADE)
   
+  def __str__(self):
+      return self.name
+    
+  def save_image(self):
+      self.save()
+ 
--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from .models import Category, Image, Location
 
-# Create your tests here.
+class CategoryTestClass(TestCase):
+   # Set up method
+    def setUp(self):
+      self.image = Category(name = 'travel')
+      
+   # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Category))  
+        
+
+class LocationTestClass(TestCase):
+   # Set up method
+    def setUp(self):
+      self.location = Location(name = 'Dubai')
+      
+   # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location,Location)) 
+        
+        
+class ImageTestClass(TestCase):
+   # Set up method
+    def setUp(self):
+      self.category = Category(name = 'travel')
+      self.category.save()
+      self.location = Location(name = 'Dubai')
+      self.location.save()
+      self.image = Image(name = 'image_name', description = 'this is an image', category = self.category, location = self.location)
+      
+   # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image)) 
+        
+    def test_save_method(self):
+      self.image.save()     
+      images = Image.objects.all()
+      self.assertTrue(len(images) > 0) 
+      
+    def tearDown(self):
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+        Image.objects.all().delete()  
