3767a3efd1fdd26a94c0e8558c69c33241dbf54f
Kasparov <ianjkiplagat@gmail.com>
2021-05-14 15:53:27 +0300

    add method to fetch all images

--- photos/views.py
+++ photos/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def welcome(request):
-    return render(request, 'index.html')
+    images = Image.objects.all()
+    return render(request, 'index.html', {"images":images})
 
 def about(request):
     return render(request, 'about.html')
